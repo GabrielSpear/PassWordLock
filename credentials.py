d4f70df9f0ee9aadf2bf61fd5f35fd66863c5d88
Gabriel <gabrieldvjspear@gmail.com>
2018-04-03 11:13:13 +0300

    Credentials

--- credentials.py
+++ credentials.py
@@ -3,11 +3,13 @@ class Credentials:
     Generates new instances of contacts
     '''
 
+
     contact_list = []
     def __init__(self,first_name,last_name,number,email):
         '''
         Create new instances of class User
         '''
+
 
         self.first_name = first_name
         self.last_name = last_name
